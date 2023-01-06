334252f88e5603beb0f4d29e5a31ff1fd9926029
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2023-01-06 12:45:27 +0300

    initialize taileind css

--- PaytomateMobile/src/App.tsx
+++ PaytomateMobile/src/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[tw`flex flex-row items-center h-8 bg-red-900 `,{
+      position: "fixed"
+    }]}>
+      <Text>Paytomate</Text>
       <StatusBar style="auto" />
     </View>
   );
