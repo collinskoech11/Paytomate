256399d3482f4e710d321ff33dda6ffbfd04c151
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2023-01-14 14:21:02 +0300

    stack navigation

--- PaytomateMobile/src/screens/Home.tsx
+++ PaytomateMobile/src/screens/Home.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import {Text, View } from "react-native"
+import {Text, View, Button } from "react-native"
 
-function Home() {
+function Home({navigation}) {
+  const pressHandler = () => {
+    navigation.navigate('Profile')
+  }
   return (
-    <div>Home</div>
+    <View>
+      <Text>Home</Text>
+      <Button title="go to Profile" onPress={pressHandler}/>
+    </View>
   )
 }
 
